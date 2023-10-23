9077a50c09b6fb751477c862666d3cfc68e79e67
spaffinge <52344911+adamstaff@users.noreply.github.com>
2023-10-23 10:59:50 +0100

    tidying up, added param for track volume

--- conantgardens.lua
+++ conantgardens.lua
@@ -22,23 +22,16 @@
 -- shift+K3: Fill notes
 --
 -- In Sample View
+-- E1: Select track
 -- E2: Sample start
 -- E3: Sample end
 -- K3: Load sample
--- shift+E1:
+-- shift+E1: Sample volume
 -- shift+E2: Start+end
--- shift+E3: Sample volume
+-- shift+E3:
 
 util = require "util"
 fileselect = require "fileselect"
-
-function copy_samples(ch, start, interval, samples)
-  for i = 1, editArea.width, 1 do
-    waveform.samples[i + currentTrack * editArea.width] = samples[i]
-  end
-  screenDirty = true
-  waveform.isLoaded[currentTrack + 1] = true
-end
 
 function init_params()
   params:add_separator('Conant Gardens')
@@ -46,12 +39,24 @@ function init_params()
   params:set_action('tracksAmount', tracksAmount_update)
   params:add_number('beatsAmount', 'number of beats', 1, 16, 8)
   params:set_action('beatsAmount', function(x) beatsAmount = x
-    totalBeats = 192*beatsAmount end )
+    totalTicks = 192*beatsAmount end )
+  params:add_group('track_volumes', 'track volumes', 8)
+  for i=1, 8, 1 do
+    params:add_number('trackVolume_'..i, 'track volume '..i)
+  end
   params:add_group('track_timings', 'track timings', 8)
   for i=1, 8, 1 do
     params:add_number('trackTiming_'..i, 'track timing '..i)
   end
+  params:add_group('sample_starts_ends', 'sample starts/ends', 16)
+  -- sample start/end points
+  for i=1, 8, 1 do
+  	params:add_number('sampStart_'..i, 'sample '..i..' start', 0.0,0.99,0.0)
+  	params:add_number('sampEnd_'..i, 'sample '..i..' end',0.0,1.0,1.0)
+	params:set_action('sampEnd_'.. i, function(x) softcut.loop_end(i, currentTrack+1 + x) end)
+  end
   params:add_group('track_samples', 'track samples', 8)
+  -- sample file locations
   for i=1, 8, 1 do
     currentTrack = i-1
     local file = "cancel"
@@ -66,14 +71,7 @@ function init_params()
       end
     }
   end
-   
-params:add_group('sample_starts_ends', 'sample starts/ends', 16)
-  -- for sample start/end points
-  for i=1, 8, 1 do
-  	params:add_number('sampStart_'..i, 'sample '..i..' start', 0.0,0.99,0.0)
-  	params:add_number('sampEnd_'..i, 'sample '..i..' end',0.0,1.0,1.0)
-params:set_action('sampEnd_'.. i, function(x) softcut.loop_end(i, currentTrack+1 + x) end)
-  end
+
   -- here, we set our PSET callbacks for save / load:
   params.action_write = function(filename,name,number)
     os.execute("mkdir -p "..norns.state.data.."/"..number.."/")
@@ -91,92 +89,26 @@ params:set_action('sampEnd_'.. i, function(x) softcut.loop_end(i, currentTrack+1
   end
   params:bang()
 end
---[[
-function loadPattern()
-  trackEvents = tab.load(_path.data.."/conantgardens/beat01.txt")
-end
-
-function savePattern()
-  tab.save(trackEvents,_path.data.."/conantgardens/beat01.txt")
-end
-]]
-
---tick along, play events
-function ticker()
-  while isPlaying do
-    --loop clock
-    if (clockPosition > totalBeats) then clockPosition = 0 end
-    --check if it's time for an event
-    for i, data in ipairs(trackEvents) do
-      --if there's an event to check
-      if (data[4] ~=nil) then
-        --offset track playhead position by track offset
-        local localTick = clockPosition - params:get('trackTiming_'..data[3]+1)
-        --check we're not out of bounds
-        if localTick > totalBeats then localTick = 0 - params:get('trackTiming_'..data[3]+1)
-        else if localTick < 0 then localTick = totalBeats - params:get('trackTiming_'..data[3]+1) end
-        end
-        --finally, play an event?
-        if (localTick == math.floor(totalBeats * (data[1]))) and data[3]+1 <= tracksAmount then
-        --todo add option for MIDI here --
--- put the playhead in position (voice, position)
-    softcut.position(data[3]+1,data[3]+1 + params:get('sampStart_'.. data[3]+1))
-          --set dynamic level
-          softcut.level(data[3]+1,data[4])
--- play from position to softcut.loop_end
-          softcut.play(data[3]+1,1)
-        end
-      end
-    end
-    --tick
-    clockPosition = clockPosition + tick
-    --wait
-    clock.sync(1/192)
-    if clockPosition % 16 == 0 then screenDirty = true end
-  end
-end
-
-function redraw_clock() ----- a clock that draws space
-  while true do ------------- "while true do" means "do this forever"
-    clock.sleep(1/15) ------- pause for a fifteenth of a second (aka 15fps)
-    if screenDirty and not weLoading then ---- only if something changed
-      redraw() -------------- redraw space
-      screen_dirty = false -- and everything is clean again
-    end
-  end
-end
-
-function sleeper()
-  clock.sleep(1)
-  weIniting = false
-end
 
 function init()
-  --weIniting = true
   weLoading = false
-  --inits
   redraw_clock_id = clock.run(redraw_clock)
   editArea = {width=120, height=56, border=4}
   --global x and y - tracks and beats to sequence:
   tracksAmount = 8
-  function tracksAmount_update(new)
-    tracksAmount = new
-    editArea.trackHeight = editArea.height / tracksAmount
-  end
+  currentTrack = 0
+  editArea.trackHeight = editArea.height / tracksAmount
   beatsAmount = 8
-  totalBeats = 192 * beatsAmount
+  totalTicks = 192 * beatsAmount
   trackEvents = {}
   --params
   init_params()
-  --end params
-  currentTrack = 0
-  --start with cursor set to 8th notes:
+  --cursor stuff
   segmentLength = 6
   resolutions = {1,2,3,4,6,8,12,16,24,32,48,64,96,128,192}
   beatCursor = 1
   currentDynamic = 1.0
-  --drawing stuff
-  editArea.trackHeight = editArea.height / tracksAmount
+  
   heldKeys = {false, false, false}
   nowPosition = {-1, -1}
   isPlaying = false
@@ -186,10 +118,6 @@ function init()
   weFilling = false
   fillStart = nil
   fillEnd = nil
-  theClock = clock.run(ticker)
-  clockPosition = 0
-  tick = 1
-  -- offset for entire track +- in 192ths
   sampleView = false
   softcut.event_render(copy_samples)
   waveform = {}
@@ -200,10 +128,6 @@ function init()
   waveform.rate = {}
   for i=1, 8, 1 do
     waveform.isLoaded[i] = false 
-    --waveform.samples[i] = {}
-    --waveform.channels[i] = 0
-    --waveform.length[i] = 0
-    --waveform.rate[i] = 0
   end
   
   --add samples
@@ -211,15 +135,7 @@ function init()
   
   -- clear buffer
   softcut.buffer_clear()
-  -- read file into buffer
-  -- buffer_read_mono (file, start_src, start_dst, dur, ch_src, ch_dst)
   for i=1, 8, 1 do
-    --local ch, length, rate = audio.file_info(file[i])
-    --local lengthInS = length * (1 / rate)
-    --if lengthInS > 1 then lengthInS = 1 end
-    --waveform.length[i] = lengthInS
-    --load file into buffer
-    --softcut.buffer_read_mono(file[i],0,i,waveform.length[i],1,1)
     -- enable voices
     softcut.enable(i,1)
     -- set voices to buffer 1
@@ -238,13 +154,21 @@ function init()
     softcut.play(i,0)
   end
 
-  -- ch, start, duration, number of samples to make
-  --softcut.render_buffer(1,0,4,editArea.width)
-
-  currentTrack = 0
+	--clock stuff
+  clockPosition = 0
+  tick = 1
+  theClock = clock.run(ticker)
   
   screenDirty = true
 
+end
+
+function copy_samples(ch, start, interval, samples)
+  for i = 1, editArea.width, 1 do
+    waveform.samples[i + currentTrack * editArea.width] = samples[i]
+  end
+  screenDirty = true
+  waveform.isLoaded[currentTrack + 1] = true
 end
 
 function load_file(file,track)
@@ -274,6 +198,74 @@ function load_file(file,track)
   weLoading = false
 end
 
+function ticker()
+  while isPlaying do
+    --loop clock
+    if (clockPosition > totalTicks) then clockPosition = 0 end
+    --check if it's time for an event
+    for i, data in ipairs(trackEvents) do
+      --if there's an event to check
+      if (data[4] ~=nil) then
+        --offset track playhead position by track offset
+        local localTick = clockPosition - params:get('trackTiming_'..data[3]+1)
+        --check we're not out of bounds
+        if localTick > totalTicks then localTick = 0 - params:get('trackTiming_'..data[3]+1)
+        else if localTick < 0 then localTick = totalTicks - params:get('trackTiming_'..data[3]+1) end
+        end
+        --finally, play an event?
+        if (localTick == math.floor(totalTicks * (data[1]))) and data[3]+1 <= tracksAmount then
+	        --todo add option for MIDI here --
+					-- put the playhead in position (voice, position)
+    			softcut.position(data[3]+1,data[3]+1 + params:get('sampStart_'.. data[3]+1))
+          --set dynamic level
+          softcut.level(data[3]+1,data[4] * params:get('trackVolume_'..data[3]+1))
+					-- play from position to softcut.loop_end
+          softcut.play(data[3]+1,1)
+        end
+      end
+    end
+    --tick
+    clockPosition = clockPosition + tick
+    --wait
+    clock.sync(1/192)
+    if clockPosition % 16 == 0 then screenDirty = true end
+  end
+end
+
+function tracksAmount_update(new)
+	tracksAmount = new
+	editArea.trackHeight = editArea.height / tracksAmount
+end
+
+function addRemoveEvents()
+  local barFraction = beatsAmount / 4
+  local position = (beatCursor - 1) / (resolutions[segmentLength] * barFraction)
+  local length = 1 / (resolutions[segmentLength] * barFraction)
+  local track = currentTrack
+  local foundOne = 0
+
+  --check for clashes, and delete event
+  -- structure: [position, length, track, dynamic] 
+  if #trackEvents > 0 then
+    for i=#trackEvents, 1, -1 do
+      if (trackEvents[i][4] ~= nil and position >= trackEvents[i][1] and position < trackEvents[i][1] + trackEvents[i][2] and currentTrack == trackEvents[i][3]) then
+        table.remove(trackEvents[i])
+        foundOne = 1
+        else if (trackEvents[i][4] ~= nil and trackEvents[i][1] >= position and trackEvents[i][1] < position + length and currentTrack == trackEvents[i][3]) then
+          table.remove(trackEvents[i])
+          foundOne = 1
+        end
+      end
+    end
+    screenDirty = true
+  end
+  --actually add a note
+  if (foundOne == 0 and not weMoving) then
+    table.insert(trackEvents, {position, length, track, currentDynamic})
+    screenDirty = true
+  end
+end
+
 function drawEvents()
 --draws a bright box for each of the events in trackEvents, so you can see what you're doing!
   for i, data in ipairs(trackEvents) do
@@ -297,34 +289,6 @@ function drawEvents()
       screen.rect(x, y, 1, h)
       screen.fill()
     end
-  end
-end
-
-function addRemoveEvents()
-  local barFraction = beatsAmount / 4
-  local position = (beatCursor - 1) / (resolutions[segmentLength] * barFraction)
-  local length = 1 / (resolutions[segmentLength] * barFraction)
-  local track = currentTrack
-  local foundOne = 0
-  
-  --check for clashes, and delete event
-  -- structure: [position, length, track, dynamic] 
-  if #trackEvents > 0 then
-    for i=#trackEvents, 1, -1 do
-      if (trackEvents[i][4] ~= nil and position >= trackEvents[i][1] and position < trackEvents[i][1] + trackEvents[i][2] and currentTrack == trackEvents[i][3]) then
-        table.remove(trackEvents[i])
-        foundOne = 1
-        else if (trackEvents[i][4] ~= nil and trackEvents[i][1] >= position and trackEvents[i][1] < position + length and currentTrack == trackEvents[i][3]) then
-          table.remove(trackEvents[i])
-          foundOne = 1
-        end
-      end
-    end
-    screenDirty = true
-  end
-  if (foundOne == 0 and not weMoving) then
-    table.insert(trackEvents, {position, length, track, currentDynamic})
-    screenDirty = true
   end
 end
 
@@ -366,7 +330,7 @@ function drawSequencer()
     screen.stroke()
   end
   --play head line, position updated by and taken from ticker() 
-  local playheadX = editArea.border + (clockPosition / totalBeats) * editArea.width
+  local playheadX = editArea.border + (clockPosition / totalTicks) * editArea.width
   screen.level(15)
   screen.move(playheadX, 0)
   screen.line(playheadX, editArea.border)
@@ -492,6 +456,21 @@ function redraw()
 
 end
 
+function redraw_clock() ----- a clock that draws space
+  while true do ------------- "while true do" means "do this forever"
+    clock.sleep(1/15) ------- pause for a fifteenth of a second (aka 15fps)
+    if screenDirty and not weLoading then ---- only if something changed
+      redraw() -------------- redraw space
+      screen_dirty = false -- and everything is clean again
+    end
+  end
+end
+
+function sleeper()
+  clock.sleep(1)
+  weIniting = false
+end
+
 function moveEvent(i,e,d)
 --takes event number, encoder number and turn amount, and moves events
   if (e == 1) then
@@ -522,6 +501,9 @@ function enc(e, d)
   if (heldKeys[1]) then
     if sampleView then
       --sample view shift behaviour
+      if e == 1 then
+      	
+      end
       if e == 2 then
   	    params:set('sampStart_'..currentTrack+1, params:get('sampStart_'..currentTrack+1) + (d/100))
    softcut.render_buffer(1,currentTrack+1 + params:get('sampStart_'..currentTrack+1), params:get('sampEnd_'..currentTrack+1) - params:get('sampStart_'..currentTrack+1), editArea.width + 1)   
